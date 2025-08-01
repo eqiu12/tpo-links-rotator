623ff26e1249269d47e7b2a89342c14200224e36
eqiu12 <150559909+eqiu12@users.noreply.github.com>
2025-08-01 16:30:40 +0300

    Remove deprecated appDir experimental flag

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // App directory is now stable in Next.js 14
-  experimental: {
-    appDir: true,
-  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
